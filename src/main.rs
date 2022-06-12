761638d5baba62c0c517b891d3eba5300859f7bb
KoyaSaeki <koyasaeki@gmail.com>
2022-06-12 16:25:17 +0900

    update: JSON を返却する

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-use axum::{routing::get, Router};
+use axum::{response::Json, routing::get, Router};
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World! " }));
+    let app = Router::new()
+        .route("/", get(plain_text))
+        .route("/json", get(json));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn plain_text() -> &'static str {
+    "Hello, World"
+}
+
+async fn json() -> Json<Value> {
+    Json(json!({ "data": 42 }))
 }
